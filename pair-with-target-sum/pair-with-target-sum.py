108f993ec8ad77606954ec35e56d9cc3d4ac6677
satanas <wil.alejandro@gmail.com>
2020-07-15 17:32:11 -0700

    Refactored pair with target sum

--- pair-with-target-sum/pair-with-target-sum.py
+++ pair-with-target-sum/pair-with-target-sum.py
@@ -2,22 +2,18 @@ def pair_with_targetsum(arr, target_sum):
     start_pointer = 0
     end_pointer = len(arr) - 1
 
-    s = arr[start_pointer]
-    e = arr[end_pointer]
+    while start_pointer < end_pointer:
+        curr_sum = arr[start_pointer] + arr[end_pointer]
 
-    while s + e != target_sum:
-        if s + e < target_sum:
+        if curr_sum == target_sum:
+            return [start_pointer, end_pointer]
+
+        if curr_sum < target_sum:
             start_pointer += 1
-        if s + e > target_sum:
+        if curr_sum > target_sum:
             end_pointer -= 1
 
-        s = arr[start_pointer]
-        e = arr[end_pointer]
-
-        if start_pointer == end_pointer:
-            return [None, None]
-
-    return [start_pointer, end_pointer]
+    return [-1, -1]
 
 if __name__ == "__main__":
     print(pair_with_targetsum([1, 2, 3, 4, 6], 6))
