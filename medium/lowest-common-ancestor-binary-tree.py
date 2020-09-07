49ccb82ef73961cae5c6a2b17f1520fc7d230571
satanas <wil.alejandro@gmail.com>
2020-09-07 10:33:25 -0700

    Trying second solution for LCA

--- medium/lowest-common-ancestor-binary-tree.py
+++ medium/lowest-common-ancestor-binary-tree.py
@@ -28,5 +28,28 @@ class Solution:
             return None
 
         return left if left is not None else right
-    
+
+class Solution2:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        path_p = self.recursive_traversal(root, p.val, [])
+        path_q = self.recursive_traversal(root, q.val, [])
+
+        min_length = min(len(path_p), len(path_q))
+        for i in range(min_length):
+            if path_p[i].val != path_q[i].val:
+                return path_p[i - 1]
+
+    def recursive_traversal(self, node, value, path):
+        if node is None:
+            return path
+
+        path.append(node)
+
+        if node.val == value:
+            return True
         
+        if self.recursive_traversal(node.left, value, path) or self.recursive_traversal(node.right, value, path):
+            return True
+        else:
+            path.pop(-1)
+            return False
